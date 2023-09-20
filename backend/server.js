a19e2859305b3daeb110d950440a53aac1c7d662
Muskaan Puri <muskaanpuri04@gmail.com>
2023-09-21 05:08:20 +0530

    connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import connectDB from './db.js'
+dotenv.config()
+
 
 const app = express()
 
 const port = process.env.PORT || 8000
 
 
-app.listen(port, () => console.log(`listening to port ${port}...`))
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI).then(() => console.log("Connected to DB..."))
+        app.listen(port, () => console.log(`listening to port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
